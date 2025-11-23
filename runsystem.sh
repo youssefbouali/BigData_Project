ed16f90708fd70bd236c577c9a08a933e5aba7d3
Bouali <youssefbouali206@gmail.com>
2025-11-23 22:23:22 +0100

    Up

--- runsystem.sh
+++ runsystem.sh
@@ -1,11 +1,38 @@
-# 1. build and run
-docker-compose up -d --build
+#!/bin/bash
 
-# 2. load data
-docker-compose exec spark-submit python /scripts/clean_and_load.py
+# Start containers
+docker-compose up -d
 
-# 3. model training
-docker-compose exec spark-submit python /scripts/train_model.py
+# Wait for Cassandra to be ready
+echo "Waiting for Cassandra to start..."
+until docker exec -it cassandra cqlsh -e "DESCRIBE KEYSPACES;" &> /dev/null; do
+    echo -n "."
+    sleep 5
+done
+echo "Cassandra is ready!"
 
-# 4. run Streaming (get from kafka)
-docker-compose exec spark-submit python /scripts/streaming_predict.py
+docker exec -it cassandra cqlsh cassandra 9042 -e "DESC KEYSPACES;" && docker exec -it cassandra cqlsh cassandra 9042 -f /init.cql && docker exec -it cassandra cqlsh cassandra 9042 -e "SELECT COUNT(*) FROM netflow.flows;" && docker exec -it bigdata_project_spark-master_1 spark-submit --master spark://spark-master:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 --conf spark.cassandra.connection.host=cassandra --conf spark.cassandra.auth.username=cassandra --conf spark.cassandra.auth.password=cassandra /scripts/clean_and_load.py
+
+
+docker exec -it cassandra cqlsh cassandra 9042 -e "SELECT * FROM netflow.flows LIMIT 20;"
+
+
+#docker exec -it cassandra cqlsh cassandra 9042 -e "SELECT * FROM netflow.flows LIMIT 10;" && docker exec -it bigdata_project_spark-master_1 pip install matplotlib pandas --break-system-packages && docker exec -it bigdata_project_spark-master_1 spark-submit --master spark://spark-master:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 --driver-memory 4g --executor-memory 2g /scripts/train_model.py
+
+chmod -R 777 ./model
+
+
+
+docker exec -it bigdata_project_spark-master_1 sh -c "pip install scapy --break-system-packages" && docker exec -it bigdata_project_spark-master_1 sh -c "cd /scripts && python3 realtime_netflow_predictor_savedb.py"
+
+
+
+
+
+docker exec -it bigdata_project_spark-master_1 curl -s https://google.com > /dev/null
+docker exec -it bigdata_project_spark-master_1 apt update && apt install -y hping3
+docker exec -it bigdata_project_spark-master_1 hping3 -S -p 80 --flood 8.8.8.8
+
+
+
+docker exec -it cassandra cqlsh cassandra 9042 -e "SELECT * FROM netflow.predictions;"
